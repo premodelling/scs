fbb44098f3cc2df0129c2b8cdafddb645518726d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-01 19:48:52 +0000

    Add R/functions/ExploreDayOccupation.R

---
+++ R/functions/ExploreDayOccupation.R
@@ -0,0 +1,22 @@
+# Title     : Day of Week & Occupation
+# Objective : Day of Week & Occupation
+# Created by:
+# Created on: 01/12/2021
+
+GraphDayOccupation <- function (survey) {
+
+  ggplot(data = survey) +
+    geom_bar(mapping = aes(x = day_of_week, fill = occupation), alpha = 0.65, na.rm = TRUE) +
+    theme_minimal() +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.spacing = unit(2, units = 'lines'),
+          strip.text.x = element_text(size = 13),
+          axis.text.x = element_text(size = 11, angle = 90),
+          axis.text.y = element_text(size = 11),
+          axis.title.x = element_text(size = 13),
+          axis.title.y = element_text(size = 13)) +
+    xlab(label = '\nday of week\n') +
+    ylab(label = 'count\n')
+
+}
