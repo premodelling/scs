6b54b24fcf0e8ba74fe2850c3e193d33f614106e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-03 19:51:12 +0000

    Update R/mapping/AreaCodeDescriptions.R

--- R/mapping/AreaCodeDescriptions.R
+++ R/mapping/AreaCodeDescriptions.R
@@ -25,32 +25,10 @@ AreaCodeDescriptions <- function (mappings) {
 
 
 
-  # United Kingdom Countries
-  ctry <- fread(file = 'data/postcodes/dictionaries/country_names_and_codes_uk.csv',
-                select = c('CTRY12CD', 'CTRY12NM'),
-                colClasses = list(character = c('CTRY12CD', 'CTRY12NM')),
-                data.table = FALSE)
-  names(ctry) <- c('ctry', 'country')
-
-
-
-  # United Kingdom Counties
-  cty <- fread(file = 'data/postcodes/dictionaries/county_names_and_codes_uk.csv',
-               header = TRUE,
-               select = c('CTY21CD', 'CTY21NM'),
-               colClasses = list(character = c('CTY21CD', 'CTY21NM')),
-               data.table = FALSE)
-  names(cty) <- c('cty', 'county')
-
-
-
   # The descriptions for the codes in mappings
   descriptions <- mappings %>%
     left_join(y = oac11, by = 'oac11') %>%
-    left_join(y = ru11ind, by = 'ru11ind' ) %>%
-    left_join(y = ctry, by = 'ctry') %>%
-    left_join(y = cty, by = 'cty') %>%
-    select(!c(ctry, cty))
+    left_join(y = ru11ind, by = 'ru11ind' )
 
   return(descriptions)
 
