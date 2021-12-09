35c0bdc1a9695aa4c31d11702706794a38da0881
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-09 01:57:33 +0000

    Update R/modelling/places.R

--- R/modelling/places.R
+++ R/modelling/places.R
@@ -52,6 +52,33 @@ census$AgeGroup <- as.factor(census$AgeGroup)
 focus <- focus %>%
   filter(age >= 35)
 
+ggplot(data = focus, aes(x = long, y = lat, col = ru11name, size = total_contacts)) +
+  geom_point(alpha = 0.85) +
+  coord_map() +
+  theme_minimal() +
+  theme(plot.caption = element_text(hjust = 0, size = 11, colour = 'darkgrey'),
+        axis.title.x = element_text(size = 13, face = 'bold'), axis.text.x = element_text(size = 11),
+        axis.title.y = element_text(size = 13, face = 'bold'), axis.text.y = element_text(size = 11)) +
+  xlab(label = '\nlongitude') +
+  ylab(label = '\nlatitude\n') +
+  guides(col = guide_legend(title = 'Rural Urban Classification'),
+         size = guide_legend(title = 'Total Contacts'))
+
+ggplot(data = focus, aes(x = long, y = lat, col = total_contacts, size = total_contacts)) +
+  geom_point(alpha = 0.85) +
+  scale_color_gradient(low = 'orange', high = 'black') +
+  coord_map() +
+  theme_minimal() +
+  theme(plot.caption = element_text(hjust = 0, size = 11, colour = 'darkgrey'),
+        axis.title.x = element_text(size = 13, face = 'bold'), axis.text.x = element_text(size = 11),
+        axis.title.y = element_text(size = 13, face = 'bold'), axis.text.y = element_text(size = 11)) +
+  xlab(label = '\nlongitude') +
+  ylab(label = '\nlatitude\n') +
+  guides(col = guide_legend(title = 'Rural Urban Classification'),
+         size = guide_legend(title = 'Total Contacts'))
+
+
+
 
 
 # Modelling
