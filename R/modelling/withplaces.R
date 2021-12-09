d440f0b0f202b964263111e6f991bf2e4fe79401
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-09 14:02:57 +0000

    Update R/modelling/withplaces.R

--- R/modelling/withplaces.R
+++ R/modelling/withplaces.R
@@ -11,6 +11,9 @@ source(file = 'R/modelling/WithPlacesGraphs.R')
 focus <- WithPlacesData()
 
 
+# correlations
+# corrplot::corrplot(cor(focus[, c("lat", "long", "total_contacts", "age")]))
+
 
 # Sample for Sex, Age Group, Population
 
@@ -28,10 +31,6 @@ quotients <- quotients %>%
 
 quotients <- left_join(x = quotients, y = census, by = c('agegroup' = 'AgeGroup'))
 
-quotients <- quotients %>%
-  mutate(reference_q <- Males/Females) %>%
-  mutate(sample_q <- male/female)
-
 
 
 # dispersion: take note of rural village & rural village in sparse setting point
@@ -41,17 +40,18 @@ CoordinatesContactsPlace(focus = focus)
 
 
 # Modelling
-modelglmnb <- glm.nb(formula = total_contacts ~ agegroup + ru11ind + occupation + household_size + sex, data = focus)
-summary(modelglmnb)
-plot(modelglmnb)
+# modelglmnb <- glm.nb(formula = total_contacts ~ agegroup + ru11ind + occupation + household_size + sex, data = focus)
+# summary(modelglmnb)
+# plot(modelglmnb)
+#
+# modelglmnb <- glm.nb(formula = total_contacts ~ agegroup + ru11ind + occupation + household_size, data = focus)
+# summary(modelglmnb)
+# plot(modelglmnb)
+#
+# modelglmnb <- glm.nb(formula = total_contacts ~ agegroup + ru11ind + occupation, data = focus)
+# summary(modelglmnb)
+# plot(modelglmnb)
 
-modelglmnb <- glm.nb(formula = total_contacts ~ agegroup + ru11ind + occupation + household_size, data = focus)
-summary(modelglmnb)
-plot(modelglmnb)
-
-modelglmnb <- glm.nb(formula = total_contacts ~ agegroup + ru11ind + occupation, data = focus)
-summary(modelglmnb)
-plot(modelglmnb)
 
 modelglmnb <- glm.nb(formula = total_contacts ~ agegroup + ru11ind + occupation + household_size + sex, data = focus)
 modelglmnbstep <- step(modelglmnb, trace = 0)
